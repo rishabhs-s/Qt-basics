b54e741bf0f4722c25851df41206358bba3577d6
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-09-08 23:37:25 +0530

    Qt-4

---
+++ qt-4/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::about(this,"title","Message box");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QMessageBox::aboutQt(this,"title");
+}
